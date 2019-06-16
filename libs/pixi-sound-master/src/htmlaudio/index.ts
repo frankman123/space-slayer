ff2006cc092dc4c318e5332d54e0f63e9cdbd66b
frankman123 <frank.cizmich@gmail.com>
2019-06-16 11:44:42 -0300

    removed junk

--- libs/pixi-sound-master/src/htmlaudio/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Classes supporting non-WebAudio based browsers.
- * @namespace PIXI.sound.htmlaudio
- * @private
- */
-export { HTMLAudioMedia } from "./HTMLAudioMedia";
-export { HTMLAudioInstance } from "./HTMLAudioInstance";
-export { HTMLAudioContext } from "./HTMLAudioContext";
--- libs/pixi-sound-master/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Utilities that work with sounds.
- * @namespace PIXI.sound.utils
- */
-export * from "./playOnce";
-export * from "./render";
-export * from "./resolveUrl";
-export * from "./sineTone";
-export * from "./supported";
